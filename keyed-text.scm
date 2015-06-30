4f96044dd0fe5a00a11ad56fbfe767630b00b1ce
Matt Gushee <matt@gushee.net>
2015-06-30 16:00:39 -0600

    Added keyed-text module.

---
+++ keyed-text.scm
@@ -0,0 +1,10 @@
+;;; keyed-text.scm -- Strings accessed by keys for RASCL.
+;;;   Copyright © 2015 by Matthew C. Gushee <matt@gushee.net>
+;;;   This program is open-source software, released under the
+;;;   BSD license. See the accompanying LICENSE file for details.
+
+(module keyed-text
+        *
+        (import scheme chicken)
+
+) ; END MODULE
